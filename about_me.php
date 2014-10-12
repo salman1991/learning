110191c8bfaf9bd33a8bf824aff1e1baac1348f9
Salman <salmanbecomps@gmail.com>
2014-10-12 16:25:37 +0530

    Created about_us page and linked to index page

---
+++ about_me.php
@@ -0,0 +1,10 @@
+<html>
+	<head>
+		<title>Learning</title>
+	</head>
+	<body>
+		<h1>About Me</h1>
+		<p>I am a software developer at Iksula.</p>
+		<a href="index.php">Home</a>
+	</body>
+</html>
--- index.php
+++ index.php
@@ -4,5 +4,7 @@
 	</head>
 	<body>
 		<h1>This is my index page</h1>
+		<p>I use this to showcase my skills</p>
+		<a href="about_me.php">View more about me</a>
 	</body>
 </html>
